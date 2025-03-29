10c8b355e37b7dbde025dcf89a314227b0254b3f
AislingHeanue <aislingheanue@gmail.com>
2025-03-29 13:49:41 +0000

    Debugging

--- vercel.sh
+++ vercel.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+echo "Installing go..."
 GO_VERSION="1.24.1"
 curl -sSL "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz" -o go.tgz
 tar -xzf go.tgz
 export PATH="$PATH:$(pwd)/go/bin"
 rm go.tgz
 
+echo "Running vite build..."
 vite build
