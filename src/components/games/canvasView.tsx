59c20fbdde1aeeeda02273714bf349d8de3a3991
AislingHeanue <aislingheanue@gmail.com>
2025-06-19 14:50:15 +0100

    Fix some spacing issues and refactor the footer to not explode

--- src/components/games/canvasView.tsx
+++ src/components/games/canvasView.tsx
@@ -14,40 +14,41 @@ export type CanvasViewProps = {
 export default function CanvasView({ title, source, controller, game }: CanvasViewProps) {
   return (
     <>
-      <div className="lg:w-[58.8%] md:w-5/6 mx-auto lg:h-full"> <div className="grid lg:grid-cols-3 lg:grid-rows-10 md:grid-cols-1 gap-4 flex h-full pt-4">
-        <div className="col-span-1 grid grid-cols-3 lg:grid-rows-10 h-full lg:row-span-10">
-          <div className="col-span-2 h-fit lg:row-span-1">
-            <Title>{title}</Title>
-          </div>
-          <div className="col-span-1 h-fit">
-            <button
-              type="button"
-              className="mb-2 fort-semibold text-white bg-stone-900 dark:bg-white dark:text-stone-900 rounded ml-1 pl-2 pr-2"
-            >
-              <Link href="/">Back Home</Link>
-            </button>
-            <button
-              type="button"
-              className="mb-2 fort-semibold text-white bg-stone-900 dark:bg-white dark:text-stone-900 rounded ml-1 pl-2 pr-2"
-            >
-              <a
-                href={source}
-                target="_blank"
-                rel="noopener"
-              >Source Code</a>
-            </button>
-          </div>
-          <div className=" w-full col-span-3 h-full overflow-hidden lg:row-span-9 bg-stone-600">
-            <div className="h-full lg:scrollbar lg:scrollbar-thumb-stone-600 lg:scrollbar-track-stone-700 lg:overflow-auto lg:pr-3 lg:overflow-y-auto col-span-3" >
-              {controller}
+      <div className="lg:w-[59.5%] md:w-5/6 mx-auto lg:h-full">
+        <div className="grid lg:grid-cols-3 lg:grid-rows-11 md:grid-cols-1 gap-4 flex h-full pt-4">
+          <div className="col-span-1 grid grid-cols-5 lg:grid-rows-11 h-full lg:row-span-11 gap-4">
+            <div className="col-span-3 h-fit lg:row-span-1">
+              <Title>{title}</Title>
+            </div>
+            <div className="col-span-2 h-fit mt-2 lg:row-span-1">
+              <button
+                type="button"
+                className="mb-2 fort-semibold text-white bg-stone-600 dark:bg-white dark:text-stone-900 rounded ml-1 w-full"
+              >
+                <Link href="/">Back Home</Link>
+              </button>
+              <button
+                type="button"
+                className="mb-2 fort-semibold text-white bg-stone-600 dark:bg-white dark:text-stone-900 rounded ml-1 pl-2 pr-2 w-full"
+              >
+                <a
+                  href={source}
+                  target="_blank"
+                  rel="noopener"
+                >Source Code</a>
+              </button>
+            </div>
+            <div className=" w-full col-span-5 h-full lg:row-span-10 bg-stone-300 dark:bg-stone-600">
+              <div className="h-full w-full lg:scrollbar lg:scrollbar-thumb-stone-600 lg:scrollbar-track-stone-700 lg:overflow-auto lg:pr-3 lg:pl-3 lg:overflow-y-auto col-span-3" >
+                {controller}
+              </div>
             </div>
           </div>
+          <div className="lg:col-span-2 lg:row-span-1"></div>
+          <div className="lg:col-span-2 lg:row-span-10 flex w-full max-h-full aspect-square">
+            {game}
+          </div>
         </div>
-        <div className="lg:col-span-2 lg:row-span-1"></div>
-        <div className="lg:col-span-2 lg:row-span-9 flex w-full max-h-full aspect-square">
-          {game}
-        </div>
-      </div>
       </div >
     </>
   );
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -8,7 +8,7 @@ import ThemeSwitcherClient from "./themeSwitcherClient";
 export default function Layout({ children, scrollable }: PropsWithChildren<{ scrollable: boolean }>) {
   return (
     <>
-      <div className={`bg-white dark:bg-stone-900 dark:text-stone-300 text-stone-900 min-h-screen font-inter lg:justify-center ${!scrollable ? "lg:h-screen lg:flex-row lg:flex lg:overflow-hidden" : ""}`} >
+      <div className={`bg-white dark:bg-stone-900 dark:text-stone-300 text-stone-900 min-h-screen font-inter lg:justify-center ${!scrollable ? "lg:h-screen lg:flex-col lg:flex" : ""}`} >
         <ThemeSwitcherClient />
         <main className={` ${!scrollable ? "lg:pb-25 lg:overflow-hidden" : ""}`}>
           {children}
