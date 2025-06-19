b3df29fb5a915e1eec5143b2665e38db42b66ac9
AislingHeanue <aislingheanue@gmail.com>
2025-06-19 15:21:27 +0100

    Change some colours back

--- src/components/games/canvasView.tsx
+++ src/components/games/canvasView.tsx
@@ -38,8 +38,8 @@ export default function CanvasView({ title, source, controller, game }: CanvasVi
                 >Source Code</a>
               </button>
             </div>
-            <div className=" w-full col-span-5 h-full lg:row-span-10 bg-stone-200 dark:bg-stone-800">
-              <div className="h-full w-full lg:scrollbar lg:scrollbar-thumb-stone-600 lg:scrollbar-track-stone-700 lg:overflow-auto lg:pr-3 lg:pl-3 lg:overflow-y-auto col-span-3" >
+            <div className=" w-full col-span-5 h-full lg:row-span-10">
+              <div className="h-full w-full lg:scrollbar lg:scrollbar-thumb-stone-600 lg:scrollbar-track-stone-700 lg:overflow-auto lg:pr-3 lg:overflow-y-auto col-span-3" >
                 {controller}
               </div>
             </div>
