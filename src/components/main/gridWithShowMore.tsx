6762de71d87416ab6c17830fcd1351b1bb25deff
AislingHeanue <aislingheanue@gmail.com>
2025-07-10 18:18:33 +0100

    Fix import

--- src/components/main/gridWithShowMore.tsx
+++ src/components/main/gridWithShowMore.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React, { useEffect, useRef, useState } from 'react';
-import AnimateHeight from 'react-animate-height';
 // import { Transition } from 'react-transition-group';
 
 interface GridWithShowMoreProps {
